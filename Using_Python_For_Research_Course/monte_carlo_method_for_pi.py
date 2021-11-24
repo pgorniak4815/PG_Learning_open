c08fa390edd973d7a78b33f6448fe860bd8cd2fe
pgorniak4815 <pawelgorniak@protonmail.com>
2021-11-25 00:15:18 +0100

    Visual little visual changes

--- Using_Python_For_Research_Course/monte_carlo_method_for_pi.py
+++ Using_Python_For_Research_Course/monte_carlo_method_for_pi.py
@@ -10,11 +10,13 @@ import random
 
 random.seed(1)
 
+
 def rand():
     """
     Return random number from -1 to 1 with uniform distribution
     """
-    return random.uniform(-1,1)
+    return random.uniform(-1, 1)
+
 
 def distance(x, y):
     """
@@ -22,17 +24,18 @@ def distance(x, y):
     """
     return(pow((y[0]-x[0])**2 + (y[1]-x[1])**2, 1/2))
 
-def in_circle(x, origin = [0]*2):
+
+def in_circle(x, origin=[0]*2):
     """
     Return true if point is in a circle with a radius of 1 and center
     in [0,0] or false if not
     """
-    if distance(x,origin)<1:
+    if distance(x, origin) < 1:
         return True
     else:
         return False
 
-R = 1000000 #number of random points (accuracy of the result)
+R = 1000000  # number of random points (accuracy of the result)
 x = []
 inside = []
 
