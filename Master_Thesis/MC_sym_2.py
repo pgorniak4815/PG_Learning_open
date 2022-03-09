ab694ce3456834d1f67ae32d7224f30d39d2021b
Pawel Gorniak <pawelgorniak@protonmail.com>
2022-03-09 16:38:49 +0100

    work on generation emission times and binning

--- Master_Thesis/MC_sym_2.py
+++ Master_Thesis/MC_sym_2.py
@@ -26,19 +26,38 @@ def binning(x, size = 10**(-9)):
 
 random.seed(100)
 number = 100000;
-emissions = np.array([]);
-
+emissions = [];
 tau = 200
 
 for i in range(number-1):
     
     t = -tau*math.log(random.uniform(0,1))
-    emissions = np.append(emissions, t)
+    emissions.append(t);
 
+x = emissions;
+size = 10;
+number_of_bins = round((max(x)-min(x))/size) + 1;
 
-print(emissions)
+bins = [i for i in range(0,number_of_bins)]
+bins = bins*size;
 
+bins_label = [(i+size)/2 for i in bins];
 
+values = [0]*len(bins_label)
+
+binned_data = dict(zip(x,values));
+
+n=0;
+
+for item in x:
+    for lim in bins:
+            if item >= lim:
+                binned_data[bins_label[n]] =+ 1;
+
+print(len(binned_data))
+
+#plt.plot(emissions_times);
+#plt.show
 
 
 
