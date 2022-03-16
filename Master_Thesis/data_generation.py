f80f0ee47d9a210fa1213bbeb0ab5f7428ff91d8
Pawel Gorniak <pawelgorniak@protonmail.com>
2022-03-16 16:03:38 +0100

    Save data to file and read to make graph

--- Master_Thesis/data_generation.py
+++ Master_Thesis/data_generation.py
@@ -2,11 +2,16 @@ import math
 import random
 import numpy as np
 
+
 random.seed(100)
+
+
 number = 1000000
+tau = 15
+
+
 emissions = []
-tau = 30
-col_names = ["Emission time", "Counts"]
+
 
 for i in range(number-1):
     
@@ -14,8 +19,7 @@ for i in range(number-1):
     emissions.append(t)
 
 emissions_np = np.array(emissions)
-
-np.savetxt("Master_Thesis\data\emissions_times.csv", emissions_np, delimiter = ",")
+np.savetxt('Master_Thesis\data\emissions_times.csv', emissions_np, delimiter = ',')
 
 
 
--- Master_Thesis/visualization.py
+++ Master_Thesis/visualization.py
@@ -1,7 +1,9 @@
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
 import my_functions as mf
+import numpy as np
 
+emissions = np.loadtxt('Master_Thesis\data\emissions_times.csv',
+                 delimiter=",", dtype=float)
 
 binned_emissions = mf.binning(emissions)
 EM = binned_emissions.items()
@@ -10,4 +12,4 @@ x, y = zip(*EM)
 
 plt.plot(x,y,'ro')
 plt.yscale('log')
-plt.savefig("graphs\mygraph.png")
+plt.savefig('Master_Thesis\graphs\mygraph.png')
